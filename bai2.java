97e3f74ec70e0bb4ed26dc1ab548086762ee5e99
Namvjppro <76437899+Namvjppro@users.noreply.github.com>
2021-05-06 10:17:17 +0700

    Update bai2.java

--- bai2.java
+++ bai2.java
@@ -3,35 +3,24 @@ public class bai2
     public static void main(String[] args)
     {
         int []a={1,4,9,10,5,9,16,9,20,25};
-        int []b={1,4,9,10,5,9,16,9,20,25};
-        for(int i=0;i<9;i++)
+        int x = a[0];
+        for(int i=0;i<8;i++)
         {
-            for(int j =i+1;j<10;j++)
+            if(a[i]!=x||i==0)
             {
-                if(b[i]>b[j])
+                int dem=1,vt=0;
+                for(int j = i+1;j<10;j++)
                 {
-                    int temp = b[i];
-                    b[i]=b[j];
-                    b[j]=temp;
-                }
-            }
-        }
-        for(int i =0;i<7;i++)
-        {
-            if(b[i]==b[i+2])
-            {
-                int dem = 0;
-                for(int j = 0;j<10;j++)
-                {
-                    if(a[j]==b[i])
-                    dem++;
-                    if(dem==3)
+                    if(a[j]==a[i])
                     {
-                        System.out.println("number "+b[i]+" has appear 3 time and index of "+b[i]+" is "+j);
-                        break;
+                        dem++;
+                        if(dem==3)
+                            vt = j;
+                        a[j]=x;
                     }
                 }
-                i=i+2;
+                if(dem==3)
+                    System.out.println("number "+a[i]+" has appear 3 time and index of "+a[i]+" is "+ vt);
             }
         }
     }
